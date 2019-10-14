2823549d3e27fcd86f5d399b8cb1889df893b867
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-14 17:29:22 +0200

    Removing static field from ParticipantTest class. Renaming methods tests in ColdMealsServerTest.  Adding test count_cold_meals_returns_2_when_2_participants_arrive_at_10_pm_and_1_arrives_at_8_pm_at_second_element

--- ColdMeals/src/test/java/ColdMealsServerTest.java
+++ ColdMeals/src/test/java/ColdMealsServerTest.java
@@ -9,20 +9,20 @@ import java.util.List;
 public class ColdMealsServerTest {
 
     @Test
-    public void count_cold_meal_returns_0_when_empty_list_of_attendants() {
+    public void count_cold_meals_returns_0_when_empty_list_of_participants() {
         List<Participant> participantList = Collections.emptyList();
         Assertions.assertThat(ColdMealsServer.countColdMeals(participantList)).isEqualTo(0);
     }
 
     @Test
-    public void count_cold_meals_returns_1_when_single_attendant_arriving_at_10_pm() {
+    public void count_cold_meals_returns_1_when_1_participant_arrives_at_10_pm() {
         List<Participant> participantSingleton = Collections.singletonList(
                 new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
         Assertions.assertThat(ColdMealsServer.countColdMeals(participantSingleton)).isEqualTo(1);
     }
 
     @Test
-    public void count_cold_meals_returns_2_when_2_attendants_one_arriving_at_10_pm_the_other_at_11_pm() {
+    public void count_cold_meals_returns_2_when_1_participant_arrives_at_10_pm_and_1_at_11_pm() {
         List<Participant> participantList = new ArrayList<>();
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 23, 0)));
@@ -30,7 +30,7 @@ public class ColdMealsServerTest {
     }
 
     @Test
-    public void count_cold_meals_returns_2_when_2_attendants_arrive_at_10_pm_and_1_arrives_at_8_pm() {
+    public void count_cold_meals_returns_2_when_2_participants_arrive_at_10_pm_and_1_arrives_at_8_pm() {
         List<Participant> participantList = new ArrayList<>();
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
@@ -40,7 +40,7 @@ public class ColdMealsServerTest {
     }
 
     @Test
-    public void count_cold_meals_returns_3_when_3_attendants_arrive_at_10_pm() {
+    public void count_cold_meals_returns_3_when_3_participants_arrive_at_10_pm() {
         List<Participant> participantList = new ArrayList<>();
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
@@ -50,7 +50,7 @@ public class ColdMealsServerTest {
     }
 
     @Test
-    public void count_cold_meals_returns_3_when_3_attendants_arrive_at_10_pm_and_1_arrives_at_8_pm() {
+    public void count_cold_meals_returns_3_when_3_participants_arrive_at_10_pm_and_1_arrives_at_8_pm() {
         List<Participant> participantList = new ArrayList<>();
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
@@ -61,7 +61,7 @@ public class ColdMealsServerTest {
     }
 
     @Test
-    public void count_cold_meals_returns_3_when_1_attendant_arrives_at_10_pm_and_2_arrive_at_8_pm() {
+    public void count_cold_meals_returns_3_when_1_participant_arrives_at_10_pm_and_2_arrive_at_8_pm() {
         List<Participant> participantList = new ArrayList<>();
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
         participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 20, 0)));
@@ -71,16 +71,21 @@ public class ColdMealsServerTest {
     }
 
     @Test
-    public void error_message_returns_error_when_single_attendant_with_no_arrival_date_time() {
+    public void error_message_returns_error_when_1_participant_with_no_check_in_date_time() {
         List<Participant> participantList = new ArrayList<>();
         participantList.add(new Participant(null));
 
         Assertions.assertThat(ColdMealsServer.errorMessage(participantList)).isEqualTo("Be careful Attendant without date !");
     }
 
+
     @Test
-    public void cold_meals_server_returns_1_when_arrival_before_9_pm_at_5_am() {
-        Participant participant = new Participant(LocalDateTime.of(2019, 10, 17, 5, 0));
-        Assertions.assertThat(ColdMealsServer.countColdMeals(Collections.singletonList(participant))).isEqualTo(0);
+    public void count_cold_meals_returns_2_when_2_participants_arrive_at_10_pm_and_1_arrives_at_8_pm_at_second_element() {
+        List<Participant> participantList = new ArrayList<>();
+        participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
+        participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 20, 0)));
+        participantList.add(new Participant(LocalDateTime.of(2019, 10, 17, 22, 0)));
+
+        Assertions.assertThat(ColdMealsServer.countColdMeals(participantList)).isEqualTo(2);
     }
 }
--- ColdMeals/src/test/java/ParticipantTest.java
+++ ColdMeals/src/test/java/ParticipantTest.java
@@ -4,17 +4,15 @@ import org.junit.Test;
 import java.time.LocalDateTime;
 
 public class ParticipantTest {
-    private static final LocalDateTime LIMIT_ARRIVAL_DATE = LocalDateTime.of(2019, 10, 17, 21, 0);
-
     @Test
     public void arrived_after_should_returns_false_when_participant_arrives_at_5_am() {
         Participant participant = new Participant(LocalDateTime.of(2019, 10, 17, 5, 0));
-        Assertions.assertThat(participant.arrivedAfter(LIMIT_ARRIVAL_DATE)).isFalse();
+        Assertions.assertThat(participant.arrivedAfter(ColdMealsServer.LIMIT_ARRIVAL_DATE)).isFalse();
     }
 
     @Test
     public void arrived_after_should_returns_true_when_participant_arrives_at_11_pm() {
         Participant participant = new Participant(LocalDateTime.of(2019, 10, 17, 23, 0));
-        Assertions.assertThat(participant.arrivedAfter(LIMIT_ARRIVAL_DATE)).isTrue();
+        Assertions.assertThat(participant.arrivedAfter(ColdMealsServer.LIMIT_ARRIVAL_DATE)).isTrue();
     }
 }
