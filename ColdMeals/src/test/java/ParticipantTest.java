bb23020c5216d600529cc8d2bde3db6dada1c4b2
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-14 17:03:44 +0200

    Remove redundant test methods from ParticipantTest class and renaming remaining methods

--- ColdMeals/src/test/java/ParticipantTest.java
+++ ColdMeals/src/test/java/ParticipantTest.java
@@ -7,26 +7,14 @@ public class ParticipantTest {
     private static final LocalDateTime LIMIT_ARRIVAL_DATE = LocalDateTime.of(2019, 10, 17, 21, 0);
 
     @Test
-    public void cold_meal_attendant_returns_false_when_arrival_before_9_pm_at_5_am() {
+    public void arrived_after_should_returns_false_when_participant_arrives_at_5_am() {
         Participant participant = new Participant(LocalDateTime.of(2019, 10, 17, 5, 0));
         Assertions.assertThat(participant.arrivedAfter(LIMIT_ARRIVAL_DATE)).isFalse();
     }
 
     @Test
-    public void cold_meal_attendant_return_true_when_arrival_after_9_pm_at_11_pm() {
+    public void arrived_after_should_returns_true_when_participant_arrives_at_11_pm() {
         Participant participant = new Participant(LocalDateTime.of(2019, 10, 17, 23, 0));
-        Assertions.assertThat(participant.arrivedAfter(LIMIT_ARRIVAL_DATE)).isTrue();
-    }
-
-    @Test
-    public void cold_meal_attendant_returns_true_when_arrival_after_9_pm_at_11_30_pm() {
-        Participant participant = new Participant(LocalDateTime.of(2019, 10, 17, 23, 30));
-        Assertions.assertThat(participant.arrivedAfter(LIMIT_ARRIVAL_DATE)).isTrue();
-    }
-
-    @Test
-    public void cold_meal_attendant_returns_true_when_arrival_after_9_pm_at_10_pm() {
-        Participant participant = new Participant(LocalDateTime.of(2019, 10, 17, 22, 0));
         Assertions.assertThat(participant.arrivedAfter(LIMIT_ARRIVAL_DATE)).isTrue();
     }
 }
