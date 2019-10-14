ae4bfc56a6b30dd8de8b2a828e9ca1c17751a331
Pierre-Henry LANGLOIS <pierre-henri.langlois@lacombedulionvert.fr>
2019-10-14 17:33:12 +0200

    ColdMeal First Test

---
+++ Socrates/Library/ColdMealChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class ColdMealChecker
+    {
+        DateTime Deadline = new DateTime(2019, 1, 1, 21, 0, 0);
+
+        public int ColdMealNumber(List<CheckIn> checkIns)
+        {
+            int coldMeals = 0;
+
+            foreach (var checkIn in checkIns)
+            {
+                coldMeals += checkIn.IsLate(Deadline) ? 1 : 0;
+            }
+            return coldMeals;
+        }
+    }
+}
---
+++ Socrates/Socrates.Tests/ColdMealTests.cs
@@ -0,0 +1,30 @@
+﻿using Library;
+using NUnit.Framework;
+using NFluent;
+using System.Collections.Generic;
+
+namespace Socrates.Tests
+{
+    [TestFixture]
+    class ColdMealTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsColdMeal()
+        {
+            var checkInList = new List<CheckIn>();
+
+            checkInList.Add(new CheckIn());
+            checkInList.Add(new CheckIn());
+            checkInList.Add(new CheckIn());
+
+            var coldMealChecker = new ColdMealChecker();
+            Check.That(coldMealChecker.ColdMealNumber(checkInList)).IsEqualTo(0);
+        }
+
+    }
+}
