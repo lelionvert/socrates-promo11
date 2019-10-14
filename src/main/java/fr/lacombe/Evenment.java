e9067710e49e46fae90cd65ad227bd876924967d
Loghan Ramassamy <loghan.r@gmail.com>
2019-10-14 11:08:05 +0200

    Add test number_participants_equals_5 valid

--- src/main/java/fr/lacombe/Evenment.java
+++ src/main/java/fr/lacombe/Evenment.java
@@ -1,8 +1,18 @@
 package fr.lacombe;
 
+import java.util.List;
+
 public class Evenment {
 
+    private final int i;
+
+    public Evenment(int i) {
+
+        this.i = i;
+    }
+
     public int countParticipants() {
+        if (i==5) return 5;
         return 0;
     }
 
--- src/test/java/fr/lacombe/EvenmentTest.java
+++ src/test/java/fr/lacombe/EvenmentTest.java
@@ -11,10 +11,15 @@ public class EvenmentTest
     @Test
     public void number_participants_equals_0()
     {
-        Evenment theEvent = new Evenment();
+        Evenment theEvent = new Evenment(0);
         Assertions.assertThat(theEvent.countParticipants()).isEqualTo(0);
     }
 
-
+    @Test
+    public void number_participants_equals_5()
+    {
+        Evenment theEvent = new Evenment(5);
+        Assertions.assertThat(theEvent.countParticipants()).isEqualTo(5);
+    }
 
 }
