6c65f3a913a13d81091517e7679b2cd24a6563da
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-14 10:57:37 +0200

    Unitary test => cold_meal_individual_returns_true_when_arrival_before_9_pm accepted

--- ColdMeals/src/test/java/TestColdMeals.java
+++ ColdMeals/src/test/java/TestColdMeals.java
@@ -1,10 +1,23 @@
-import org.junit.Assert;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
 
 public class TestColdMeals {
 
     @Test
-    public void firstTest(){
-        Assert.assertTrue(true);
+    public void cold_meal_individual_returns_true_when_arrival_before_9_pm(){
+        Attendant attendant = new Attendant(LocalDateTime.of(2019,10,17,5,0));
+        Assertions.assertThat(attendant.getColdMeal()).isTrue();
+    }
+
+    private class Attendant {
+        public Attendant(LocalDateTime arrivalDateTime) {
+
+        }
+
+        public boolean getColdMeal() {
+            return true;
+        }
     }
 }
