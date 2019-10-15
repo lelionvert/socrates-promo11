f5630648dec282db5051cc485b5c56f97829c38c
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-15 12:09:23 +0200

    Rename class test

---
+++ RegistrationPrice/src/test/java/RegistrationPriceTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RegistrationPriceTest {
+    @Test
+    public void validate_environment() {
+        assertThat(1).isEqualTo(1);
+    }
+}
--- RegistrationPrice/src/test/java/TestRegistrationPrice.java
+++
@@ -1,9 +0,0 @@
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-class TestRegistrationPrice {
-    @Test
-    void validate_environment() {
-        Assertions.assertThat(1).isEqualTo(1);
-    }
-}
