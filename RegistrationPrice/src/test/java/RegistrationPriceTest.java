c7794efb042882240818de1d593ec80f6f1dab4b
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-15 15:10:24 +0200

    add acceptance test+ Unitary Test => registration_date_should_return_correct_day_of_week_when_created accepted

--- RegistrationPrice/src/test/java/RegistrationPriceTest.java
+++ RegistrationPrice/src/test/java/RegistrationPriceTest.java
@@ -1,10 +1,37 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RegistrationPriceTest {
     @Test
-    public void validate_environment() {
-        assertThat(1).isEqualTo(1);
+    @Disabled
+    public void validate_scenario_1() {
+        DayOfWeek checkInDay = DayOfWeek.THURSDAY;
+        int checkInHour = 21;
+        int checkInMinutes = 0;
+
+        DayOfWeek checkOutDay = DayOfWeek.SUNDAY;
+        int checkOutHour = 15;
+        int checkOutMinutes = 0;
+
+        RegistrationDate checkInDate = new RegistrationDate(checkInDay, checkInHour, checkInMinutes);
+        RegistrationDate checkOutDate = new RegistrationDate(checkOutDay, checkOutHour, checkOutMinutes);
+
+        String choice = "TRIPLE";
+
+        CheckedInParticipant victoria = new CheckedInParticipant(checkInDate, checkOutDate, choice);
+
+        assertThat(SocratesRegistrationPricer.chargesTotalAmount(victoria)).isEqualTo(410);
+    }
+
+
+    @Test
+    public void registration_date_should_return_correct_day_of_week_when_created() {
+        RegistrationDate date = new RegistrationDate(DayOfWeek.FRIDAY, 0, 0);
+
+        assertThat(date.getDay()).isEqualTo(DayOfWeek.FRIDAY);
     }
 }
