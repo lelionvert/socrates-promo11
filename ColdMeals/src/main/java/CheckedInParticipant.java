9f7aa66155e06d3b9902b572db78d54ebd5444ad
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-15 10:21:10 +0200

    Make DateTimeFormatter static constant

--- ColdMeals/src/main/java/CheckedInParticipant.java
+++ ColdMeals/src/main/java/CheckedInParticipant.java
@@ -3,14 +3,14 @@ import java.time.format.DateTimeFormatter;
 
 public class CheckedInParticipant {
     private final LocalDateTime checkInDateTime;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH'h'mm");
 
     public CheckedInParticipant(LocalDateTime checkInDateTime) {
         this.checkInDateTime = checkInDateTime;
     }
 
     public CheckedInParticipant(String stringDateTime) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH'h'mm");
-        checkInDateTime = LocalDateTime.parse(stringDateTime, dateTimeFormatter);
+        checkInDateTime = LocalDateTime.parse(stringDateTime, DATE_TIME_FORMATTER);
     }
 
     public boolean checkedInAfter(LocalDateTime arrivalDate) {
