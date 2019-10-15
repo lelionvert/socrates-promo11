61a7fe00aad0adcb99430d627eb605a918126be0
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-15 11:49:22 +0200

    Add dummy test to validate environment

---
+++ RegistrationPrice/src/test/java/TestRegistrationPrice.java
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestRegistrationPrice {
+    @Test
+    void validate_environment() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
