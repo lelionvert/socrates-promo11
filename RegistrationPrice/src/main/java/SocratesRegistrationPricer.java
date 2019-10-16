7a1c406ea1710187368b6347a195da900bc8b527
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-16 11:48:41 +0200

    Generalize calculation of the refund

--- RegistrationPrice/src/main/java/SocratesRegistrationPricer.java
+++ RegistrationPrice/src/main/java/SocratesRegistrationPricer.java
@@ -1,13 +1,15 @@
 import java.time.DayOfWeek;
 
 class SocratesRegistrationPricer {
-    static int chargesTotalAmount(CheckedInParticipant victoria) {
-        if (victoria.getChoice() == ChoiceAccommodation.SINGLE) {
-            return 570;
-        }
-        if (!victoria.getCheckInDay().equals(DayOfWeek.THURSDAY)) {
-            return 470;
-        }
-        return victoria.getBasicChoicePrice();
+    static int chargesTotalAmount(CheckedInParticipant participant) {
+        return participant.getBasicChoicePrice() - getRefundAmountOfPrice(participant);
+    }
+
+    private static int getRefundAmountOfPrice(CheckedInParticipant participant) {
+        int reduction = 0;
+        if (!participant.getCheckInDay().equals(DayOfWeek.THURSDAY)
+                || !participant.getCheckOutDay().equals(DayOfWeek.SUNDAY))
+            reduction = 40;
+        return reduction;
     }
 }
