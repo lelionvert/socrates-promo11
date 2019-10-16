9b502c75ce48c57b03403a2d31e72788e8308ffa
Loghan Ramassamy <loghan.r@gmail.com>
2019-10-16 10:31:06 +0200

    Add scenario test scenario_complete_price_jp_no_accomodation_choice_arrives_thursday_and_leaves_sunday Valid

--- RegistrationPriceCalculation/src/main/java/fr/lacombe/Participant.java
+++ RegistrationPriceCalculation/src/main/java/fr/lacombe/Participant.java
@@ -2,10 +2,19 @@ package fr.lacombe;
 
 public class Participant {
 
-    public Participant(String victoria, CheckIn victoriaCheckIn, CheckOut victoriaCheckOut, Choice choice) {
+    private String name;
+    private CheckIn checkIn;
+    private CheckOut checkOut;
+    private Choice choice;
+
+    public Participant(String name, CheckIn checkIn, CheckOut checkOut, Choice choice) {
+        this.name = name;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.choice = choice;
     }
 
     public Choice getChoice() {
-        return Choice.TRIPLE;
+        return this.choice;
     }
 }
--- RegistrationPriceCalculation/src/test/java/fr/lacombe/ScenariosTest.java
+++ RegistrationPriceCalculation/src/test/java/fr/lacombe/ScenariosTest.java
@@ -7,13 +7,23 @@ public class ScenariosTest
 {
 
     @Test
-    public void scenario_complete_price_victoria_arrives_thursday_and_leaves_sunday()
+    public void scenario_complete_price_victoria_triple_choice_arrives_thursday_and_leaves_sunday()
     {
         CheckIn victoriaCheckIn = new CheckIn("2019-10-17 21:00");
         CheckOut victoriaCheckOut = new CheckOut("2019-10-20 15:00");
         Participant victoria = new Participant("Victoria", victoriaCheckIn, victoriaCheckOut, Choice.TRIPLE);
 
         assertThat((new CalculatorPrice().calculatePrice(victoria.getChoice()))).isEqualTo(410);
+    }
+
+    @Test
+    public void scenario_complete_price_jp_no_accomodation_choice_arrives_thursday_and_leaves_sunday()
+    {
+        CheckIn jpCheckIn = new CheckIn("2019-10-17 18:00");
+        CheckOut jpCheckOut = new CheckOut("2019-10-20 14:00");
+        Participant jp = new Participant("J-P", jpCheckIn, jpCheckOut, Choice.NO_ACCOMMODATION);
+
+        assertThat((new CalculatorPrice().calculatePrice(jp.getChoice()))).isEqualTo(240);
     }
 
 
