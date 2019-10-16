51bcdabcb7ac95ce821043ceb102d2c3316c4098
Loghan Ramassamy <loghan.r@gmail.com>
2019-10-16 10:06:30 +0200

    Add test choice_single_price_is_610 valid

--- RegistrationPriceCalculation/src/main/java/fr/lacombe/CalculatorPrice.java
+++ RegistrationPriceCalculation/src/main/java/fr/lacombe/CalculatorPrice.java
@@ -2,6 +2,7 @@ package fr.lacombe;
 
 public class CalculatorPrice {
     public int calculatePrice(Choice choice) {
+        if(choice == Choice.SINGLE) return 610;
         return 410;
     }
 }
--- RegistrationPriceCalculation/src/test/java/fr/lacombe/CalculatorPriceTest.java
+++ RegistrationPriceCalculation/src/test/java/fr/lacombe/CalculatorPriceTest.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 public class CalculatorPriceTest {
 
     @Test
-    public void calculate_price_of_a_choice() {
+    public void choice_triple_price_is_410() {
         Assertions.assertThat(new CalculatorPrice().calculatePrice(Choice.TRIPLE)).isEqualTo(410);
+    }
+
+    @Test
+    public void choice_single_price_is_610() {
+        Assertions.assertThat(new CalculatorPrice().calculatePrice(Choice.SINGLE)).isEqualTo(610);
     }
 }
